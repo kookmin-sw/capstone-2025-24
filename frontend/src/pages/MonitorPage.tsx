ab1e4cf0b9878a2d66822c9cffbc81d04ddcfd43
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2025-03-14 16:58:03 +0900

    feat: 라우터 설정 및 mui provider 적용 #9

---
+++ frontend/src/pages/MonitorPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MonitorPage: React.FC = () => {
+  return (
+    <div>
+      <h1>MonitorPage Page</h1>
+      {/* 라우터 파일에서 예시로 쓰이기 위해서 만들어 놓은 파일입니다. */}
+    </div>
+  );
+};
+
+export default MonitorPage;
