49f5863f0f6d413bf5366806d62e1afddd1546ae
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2025-03-15 00:48:17 +0900

    remove: alias 테스트를 위한 파일 삭제 #9

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import React from 'react';
 import MonitorPage from '../pages/MonitorPage';
-import Test from '../pages/1/2/3/4/5/test';
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MonitorPage />} />
-        <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter>
   );
