f1c8dddfd58d23214b73d8b609269e942a2e8f85
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2025-03-15 21:06:53 +0900

    fix: vite.config.ts 수정 #9

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(process.cwd(), 'src'),
     },
   },
 });
