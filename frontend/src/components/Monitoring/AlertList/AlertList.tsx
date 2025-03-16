20500912276eb7c8b265c9744cac73ab7eec2276
seo0o519 <seoyoung344@naver.com>
2025-03-16 21:14:38 +0900

    chore: 알림리스트 목데이터 생성 #12

--- frontend/src/components/Monitoring/AlertList/AlertList.tsx
+++ frontend/src/components/Monitoring/AlertList/AlertList.tsx
@@ -2,19 +2,45 @@ import React from 'react';
 import * as S from './style.ts';
 import AlertItem from './AlertItem.tsx';
 
+const mockdata = [
+  {
+    id: 1,
+    category: '폭행',
+    date: '2024.10.10. 10:10:30',
+    address: '서울특별시 성북구 보국문로 31',
+    state: "미확인"
+  },
+  {
+    id: 2,
+    category: '화재',
+    date: '2024.10.10. 10:10:30',
+    address: '서울특별시 성북구 보국문로 31 서울특별시 성북구 보국문로 31',
+    state: "미확인"
+  },
+  {
+    id: 3,
+    category: '군중 밀집',
+    date: '2024.10.10. 10:10:30',
+    address: '서울특별시 성북구 보국문로 31',
+    state: "미확인"
+  },
+  {
+    id: 4,
+    category: '쓰러짐',
+    date: '2024.10.10. 10:10:30',
+    address: '서울특별시 성북구 보국문로 31',
+    state: "확인"
+  },
+];
+
 const AlertList: React.FC = () => {
   return (
     <S.AlertListLayout>
       <S.TitleDiv>알림 리스트</S.TitleDiv>
       <S.AlertContainer>
-        <AlertItem/>
-        <AlertItem/>
-        <AlertItem/>
-        <AlertItem/>
-        <AlertItem/>
-        <AlertItem/>
-        <AlertItem/>
-        <AlertItem/>
+        {mockdata.map((alert) => (
+          <AlertItem key={alert.id} category={alert.category} date={alert.date} address={alert.address} state={alert.state} />
+        ))}
       </S.AlertContainer>
     </S.AlertListLayout>
   );
