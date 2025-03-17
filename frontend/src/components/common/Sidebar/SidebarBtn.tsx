f7abd4c7578339e4f48a9e7f028bc5e472af75b0
Chaeyeong Lee <pop98149814@gmail.com>
2025-03-17 11:24:20 +0900

    feat: 사이드바 버튼 컴포넌트 생성 및 이벤트 적용 #18

---
+++ frontend/src/components/common/Sidebar/SidebarBtn.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import * as S from './style';
+
+interface SidebarBtnProps {
+  text: string;
+  icon: string;
+  icon_focused: string;
+  onClick: (value: string) => void;
+  size: string;
+}
+
+const SidebarBtn = ({ text, icon, icon_focused, onClick, size }: SidebarBtnProps) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const handleClick = () => {
+    onClick(text);
+  };
+  return (
+    <S.SidebarBtnLayout onClick={handleClick} onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)}>
+      <img alt="icon" src={isFocused? icon_focused:icon} style={{ width: size, height: size }} />
+      <span>{text}</span>
+    </S.SidebarBtnLayout>
+  );
+};
+
+export default SidebarBtn;
