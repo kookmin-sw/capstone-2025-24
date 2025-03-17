d8466dafe58d26907412fab454d4127532d810be
Chaeyeong Lee <pop98149814@gmail.com>
2025-03-17 09:35:31 +0900

    feat: profile div 구현 #18

---
+++ frontend/src/constants/sidebarList.ts
@@ -0,0 +1,18 @@
+import { BiSolidCctv } from "react-icons/bi";
+
+export const SIDEBAR_LIST = [
+  {
+    text: '실시간 모니터링',
+    icon: 'tmp',
+  },
+  {
+    text: '사건 기록 조회',
+    icon: 'tmp',
+  },
+  {
+    text: '통계 차트',
+    icon: 'tmp',
+  },
+];
+
+export type SidebarListTypes = typeof SIDEBAR_LIST;
