b0350f5823b8e332d1eb9f63df5d51e501803141
seo0o519 <seoyoung344@naver.com>
2025-03-18 14:52:28 +0900

    add: 모달 관련 파일 생성 #23

---
+++ frontend/src/components/IncidentHistory/DetailsModal/Information.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './style.ts';
+
+const Information: React.FC = () => {
+  return (
+    <S.InformationLayout>
+      <S.NameDiv>위치</S.NameDiv>
+    </S.InformationLayout>
+  );
+};
+
+export default Information;
