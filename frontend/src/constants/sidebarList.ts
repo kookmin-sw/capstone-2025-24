d424164281f6645d9e8679ecffbc910195138cf5
Chaeyeong Lee <pop98149814@gmail.com>
2025-03-19 02:31:23 +0900

    rename: 폴더 이름 icons로 변경 #18

--- frontend/src/constants/sidebarList.ts
+++ frontend/src/constants/sidebarList.ts
@@ -1,24 +1,20 @@
-import  CCTV  from "../assets/svgs/cctv.svg";
-import  CCTV_SOLID  from "../assets/svgs/cctv_solid.svg";
-import  CHART  from "../assets/svgs/chart.svg";
-import  CHART_SOLID  from "../assets/svgs/chart_solid.svg";
-import  DOCU  from "../assets/svgs/docu.svg";
-import  DOCU_SOLID  from "../assets/svgs/docu_solid.svg";
+import CCTV from '@/assets/icons/cctv.svg';
+import CCTV_SOLID from '@/assets/icons/cctv_solid.svg';
+import CHART from '@/assets/icons/chart.svg';
+import CHART_SOLID from '@/assets/icons/chart_solid.svg';
+import DOCU from '@/assets/icons/docu.svg';
+import DOCU_SOLID from '@/assets/icons/docu_solid.svg';
 
 export const SIDEBAR_LIST = [
-  { 
+  {
     id: 1,
     text: '실시간 모니터링',
     icon: CCTV,
     icon_focused: CCTV_SOLID,
   },
-  { id: 2,
-    text: '사건 기록 조회',
-    icon: DOCU,
-    icon_focused: DOCU_SOLID,
-  },
+  { id: 2, text: '사건 기록 조회', icon: DOCU, icon_focused: DOCU_SOLID },
   {
-    id:3,
+    id: 3,
     text: '통계 차트',
     icon: CHART,
     icon_focused: CHART_SOLID,
