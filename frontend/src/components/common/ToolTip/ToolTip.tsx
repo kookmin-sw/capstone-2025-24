4a2424eb673877b296566fdb2c838abeda2f42b7
seo0o519 <seoyoung344@naver.com>
2025-03-19 13:16:01 +0900

    feat: 툴팁 컴포넌트화 #12

--- frontend/src/components/common/ToolTip/ToolTip.tsx
+++ frontend/src/components/common/ToolTip/ToolTip.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import * as S from './style.ts';
 import { styled } from '@mui/material/styles';
 import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
@@ -12,23 +11,16 @@ const BootstrapTooltip = styled(({ className, ...props }: TooltipProps) => (
   [`& .${tooltipClasses.tooltip}`]: {
     backgroundColor: theme.palette.common.white,
     color: theme.palette.common.black,
-    boxShadow: "2px 2px 10px rgba(0, 0, 0, 0.2)",
-    borderRadius: "10px",
-    padding: "10px",
+    boxShadow: '2px 2px 10px rgba(0, 0, 0, 0.2)',
+    borderRadius: '10px',
+    padding: '10px',
   },
 }));
 
-const ToolTip: React.FC = () => {
+const ToolTip = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <BootstrapTooltip
-        title={
-          <div>
-            <S.TooltipDiv>각 알림은 24시간이 지나면</S.TooltipDiv>
-            <S.TooltipDiv>알림 리스트에서 자동 삭제됩니다.</S.TooltipDiv>
-          </div>
-        }
-      >
+      <BootstrapTooltip title={<S.TooltipP>{children}</S.TooltipP>}>
         <S.InfoIcon />
       </BootstrapTooltip>
     </div>
