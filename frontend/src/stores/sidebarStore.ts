5e2af4355a8c1544f8367928763f412449f905a2
Chaeyeong Lee <pop98149814@gmail.com>
2025-03-19 10:05:45 +0900

    fix: 새로고침 시 사이드바 바뀌는 문제 해결 #18

--- frontend/src/stores/sidebarStore.ts
+++ frontend/src/stores/sidebarStore.ts
@@ -1,13 +1,37 @@
-import {create} from "zustand";
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+// import { useLocation } from 'react-router-dom';
+
+// const pageDefaultValue = ():number => {
+//   const location = useLocation().pathname;
+//   switch (location) {
+//     case "/history":
+//         return 2;
+//     case "/chart":
+//         return 3;
+//     default:
+//         return 1;
+//   }
+// };
+
 
 interface sidebarStoreType {
-    page: number;
-    setPage: (updater: (prev: number) => number) => void;
+  page: number;
+  setPage: (updater: (prev: number) => number) => void;
 }
-const sidebarStore = create<sidebarStoreType>((set)=> ({
-    page: 1,
-    // 함수형 패턴 적용해서 비동기 문제 해결
-    setPage: (updater) => set((state)=>({page: updater(state.page)})),
-}));
 
-export default sidebarStore;
+const useSidebarStore = create<sidebarStoreType>()(
+  persist(
+    (set) => ({
+      page: 1, // 기본값 (초기 페이지)
+      setPage: (updater) => set((state) => ({ page: updater(state.page) })),
+    }),
+    {
+      name: 'sidebar-storage', // zustand localstorage 사용용
+    }
+  )
+);
+
+export default useSidebarStore;
+
+
