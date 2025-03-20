054c6d3d9f154beaaad784c5863900f15cf72be8
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2025-03-20 12:14:55 +0900

    feat: 출동중인 사건 mock data #35

--- frontend/src/components/Monitoring/InProgress/InProgress.tsx
+++ frontend/src/components/Monitoring/InProgress/InProgress.tsx
@@ -1,30 +1,33 @@
-import { IoMdCamera } from "react-icons/io";
+import { InProgresstData } from '../../../mocks/InProgressData.tsx';
+import { IoMdCamera } from 'react-icons/io';
 import * as S from './InProgress.style.ts';
 
 const InProgress = () => {
   return (
     <S.InProgressLayout>
-      <S.Title>출동중인 사건 (총 8건)</S.Title>
+      <S.Title>출동중인 사건 (총 {InProgresstData.length}건)</S.Title>
       <S.CardGrid>
-        <S.Card>
-          <S.CardHeader>
-            <S.CardTitle>흉기난동 감지</S.CardTitle>
-            <S.VideoButton>
-              <IoMdCamera className="icon" />
-              영상 확인
-            </S.VideoButton>
-          </S.CardHeader>
-          <S.Line />
-          <S.CardBody>
-            <S.LocationTitle>발생 위치</S.LocationTitle>
-            <S.LocationAddress>서울특별시 성북구 보국문로 35길 21-54</S.LocationAddress>
-            <S.ResolveButton>사건 해결</S.ResolveButton>
-          </S.CardBody>
-          <S.CardFooter>
-            <span>2025.02.28 15:43:36</span>
-            <span>김하나 순경</span>
-          </S.CardFooter>
-        </S.Card>
+        {InProgresstData.map((incident) => (
+          <S.Card key={incident.id}>
+            <S.CardHeader>
+              <S.CardTitle>{incident.title} 감지</S.CardTitle>
+              <S.VideoButton>
+                <IoMdCamera className="icon" />
+                영상 확인
+              </S.VideoButton>
+            </S.CardHeader>
+            <S.Line />
+            <S.CardBody>
+              <S.LocationTitle>발생 위치</S.LocationTitle>
+              <S.LocationAddress>{incident.location}</S.LocationAddress>
+              <S.ResolveButton>사건 해결</S.ResolveButton>
+            </S.CardBody>
+            <S.CardFooter>
+              <span>{incident.dateTime}</span>
+              <span>{incident.police} 순경</span>
+            </S.CardFooter>
+          </S.Card>
+        ))}
       </S.CardGrid>
     </S.InProgressLayout>
   );
---
+++ frontend/src/mocks/InProgressData.tsx
@@ -0,0 +1,59 @@
+export interface Incident {
+  id: number;
+  title: string;
+  location: string;
+  dateTime: string;
+  police: string;
+}
+
+export const InProgresstData: Incident[] = [
+  {
+    id: 1,
+    title: '흉기난동',
+    location: '서울특별시 성북구 보국문로 35길 21-54',
+    dateTime: '2025.02.28 15:43:36',
+    police: '김하나',
+  },
+  {
+    id: 2,
+    title: '폭행',
+    location: '서울특별시 강남구 테헤란로 87길 12',
+    dateTime: '2025.02.28 14:20:10',
+    police: '박준영',
+  },
+  {
+    id: 3,
+    title: '화재',
+    location: '서울특별시 마포구 홍익로 5길 9-7',
+    dateTime: '2025.02.28 13:55:22',
+    police: '이수민',
+  },
+  {
+    id: 4,
+    title: '화재',
+    location: '서울특별시 성북구 보국문로 35길 21-54',
+    dateTime: '2025.02.28 15:43:36',
+    police: '김하나',
+  },
+  {
+    id: 5,
+    title: '군중밀집',
+    location: '서울특별시 강남구 테헤란로 87길 12',
+    dateTime: '2025.02.28 14:20:10',
+    police: '박준영',
+  },
+  {
+    id: 6,
+    title: '폭행',
+    location: '서울특별시 마포구 홍익로 5길 9-7',
+    dateTime: '2025.02.28 13:55:22',
+    police: '이수민',
+  },
+  {
+    id: 7,
+    title: '폭행',
+    location: '서울특별시 마포구 홍익로 5길 9-7',
+    dateTime: '2025.02.28 13:55:22',
+    police: '이수민',
+  },
+];
