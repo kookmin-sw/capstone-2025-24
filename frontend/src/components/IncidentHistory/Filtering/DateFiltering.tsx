f120a3f25e6f7bafe70cc87635bb629a41c86438
seo0o519 <seoyoung344@naver.com>
2025-03-25 22:26:19 +0900

    fix: datepicker 달력 열림/닫힘 컨트롤 오류 수정 #23

--- frontend/src/components/IncidentHistory/Filtering/DateFiltering.tsx
+++ frontend/src/components/IncidentHistory/Filtering/DateFiltering.tsx
@@ -1,8 +1,9 @@
 import * as S from './Filtering.style.ts';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { ko } from 'date-fns/locale';
 import { Locale } from 'date-fns';
 import DatePicker from 'react-datepicker';
+import useOutsideClick from '../../../hooks/useOutsideClick.ts';
 
 interface DateFilteringProps {
   startDate: Date;
@@ -15,18 +16,26 @@ const DateFiltering = ({ startDate, endDate, setStartDate, setEndDate }: DateFil
   const [isStartOpen, setIsStartOpen] = useState(false);
   const [isEndOpen, setIsEndOpen] = useState(false);
 
+  const startRef = useRef<HTMLDivElement>(null);
+  const endRef = useRef<HTMLDivElement>(null);
+  useOutsideClick(startRef, () => setIsStartOpen(false));
+  useOutsideClick(endRef, () => setIsEndOpen(false));
+
   return (
     <S.DateFilteringContainer>
       {/* 시작 날짜 */}
-      <S.DatePickerWrapper className={`date-picker-wrapper ${isStartOpen ? 'open' : ''}`} isOpen={isStartOpen}>
+      <S.DatePickerWrapper
+        ref={startRef}
+        className={`date-picker-wrapper ${isStartOpen ? 'open' : ''}`}
+        onClick={() => setIsStartOpen(!isStartOpen)}
+        isOpen={isStartOpen}
+      >
         <S.CalendarIcon />
         <DatePicker
           className="custom-datepicker"
           locale={ko as Locale}
           dateFormat="yyyy.MM.dd"
           selected={startDate}
-          onCalendarOpen={() => setIsStartOpen(true)}
-          onCalendarClose={() => setIsStartOpen(false)}
           onChange={(date: Date | null) => {
             if (date instanceof Date) {
               setStartDate(date);
@@ -35,21 +44,24 @@ const DateFiltering = ({ startDate, endDate, setStartDate, setEndDate }: DateFil
           selectsStart
           startDate={startDate}
           endDate={endDate}
-          placeholderText="시작 날짜"
+          open={isStartOpen}
         />
         <S.DownIcon isOpen={isStartOpen} />
       </S.DatePickerWrapper>
 
       {/* 종료 날짜 */}
-      <S.DatePickerWrapper className={`date-picker-wrapper ${isEndOpen ? 'open' : ''}`} isOpen={isEndOpen}>
+      <S.DatePickerWrapper
+        ref={endRef}
+        className={`date-picker-wrapper ${isEndOpen ? 'open' : ''}`}
+        onClick={() => setIsEndOpen(!isEndOpen)}
+        isOpen={isEndOpen}
+      >
         <S.CalendarIcon />
         <DatePicker
           className="custom-datepicker"
           locale={ko as Locale}
           dateFormat="yyyy.MM.dd"
           selected={endDate}
-          onCalendarOpen={() => setIsEndOpen(true)}
-          onCalendarClose={() => setIsEndOpen(false)}
           onChange={(date: Date | null) => {
             if (date instanceof Date) {
               setEndDate(date);
@@ -60,7 +72,7 @@ const DateFiltering = ({ startDate, endDate, setStartDate, setEndDate }: DateFil
           endDate={endDate}
           minDate={startDate}
           maxDate={new Date()}
-          placeholderText="종료 날짜"
+          open={isEndOpen}
         />
         <S.DownIcon isOpen={isEndOpen} />
       </S.DatePickerWrapper>
