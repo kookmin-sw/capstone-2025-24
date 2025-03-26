be1a4720cfc2097a3fc1a74f6826a5374b7a65a5
seo0o519 <seoyoung344@naver.com>
2025-03-26 09:18:07 +0900

    chore: 지도 이동/확대/축소 막기 #23

--- frontend/src/components/IncidentHistory/IncidentDetailsModal/Map.tsx
+++ frontend/src/components/IncidentHistory/IncidentDetailsModal/Map.tsx
@@ -4,7 +4,7 @@ import pinIcon from '@/assets/icons/pinIcon.svg';
 
 const Map = () => {
   return (
-    <S.MapDiv id="map" center={{ lat: 37.5665, lng: 126.978 }} level={9}>
+    <S.MapDiv id="map" center={{ lat: 37.5665, lng: 126.978 }} level={4} draggable={false}>
       <MapMarker
         position={{ lat: 37.5665, lng: 126.978 }}
         image={{
