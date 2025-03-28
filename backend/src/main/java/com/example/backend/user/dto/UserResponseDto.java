f54f06fa432013b76cbbcd3fc41b54da08abf27f
mjk25 <winsomjk@gmail.com>
2025-03-29 00:36:24 +0900

    fix: add officeName & rank #80

--- backend/src/main/java/com/example/backend/user/dto/UserResponseDto.java
+++ backend/src/main/java/com/example/backend/user/dto/UserResponseDto.java
@@ -1,13 +1,26 @@
 package com.example.backend.user.dto;
 
+import com.example.backend.common.domain.PoliceEntity;
+import com.example.backend.common.domain.PoliceEntity.police_rank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class UserResponseDto {
-    private Integer id;
     private Integer officeId;
     private String name;
+    private String officeName;
     private String profileUrl;
+    private police_rank rank;
+
+    public static UserResponseDto fromEntity(PoliceEntity police) {
+        return new UserResponseDto(
+                police.getOffice().getId(),
+                police.getName(),
+                police.getOffice().getName(),
+                police.getProfileUrl(),
+                police.getRank()
+        );
+    }
 }
