80157da77dbc223e747e7d40170badc8e6c8978b
mjk25 <winsomjk@gmail.com>
2025-03-29 00:48:49 +0900

    feat: add police id and JsonIgnore #80

--- backend/src/main/java/com/example/backend/user/dto/UserResponseDto.java
+++ backend/src/main/java/com/example/backend/user/dto/UserResponseDto.java
@@ -2,12 +2,15 @@ package com.example.backend.user.dto;
 
 import com.example.backend.common.domain.PoliceEntity;
 import com.example.backend.common.domain.PoliceEntity.police_rank;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class UserResponseDto {
+    @JsonIgnore
+    private Integer id;
     private Integer officeId;
     private String name;
     private String officeName;
@@ -16,6 +19,7 @@ public class UserResponseDto {
 
     public static UserResponseDto fromEntity(PoliceEntity police) {
         return new UserResponseDto(
+                police.getId(),
                 police.getOffice().getId(),
                 police.getName(),
                 police.getOffice().getName(),
