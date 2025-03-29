96776331b5243f741d4c120522b3664f3f3b9b6d
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2025-03-30 00:23:35 +0900

    chore: https 서버 설정 #61

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
 import path from 'path';
 
 export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'cert/localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'cert/localhost.pem')),
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
