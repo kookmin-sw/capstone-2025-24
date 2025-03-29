77c25a0d60c44245e13e64fcb787ef8ae3c936bf
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2025-03-30 01:15:38 +0900

    fix: 로컬 환경에서만 https 적용 #61

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,18 +2,24 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
 import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const isLocal = process.env.VITE_LOCAL_DEV === 'true';
 
 export default defineConfig({
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'cert/localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'cert/localhost.pem')),
-    },
-  },
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(process.cwd(), 'src'),
     },
   },
+  server: isLocal
+    ? {
+        https: {
+          key: fs.readFileSync(path.resolve(__dirname, 'cert/localhost-key.pem')),
+          cert: fs.readFileSync(path.resolve(__dirname, 'cert/localhost.pem')),
+        },
+      }
+    : {},
 });
