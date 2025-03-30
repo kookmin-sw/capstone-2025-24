e2edaa7c6c15534f1e0f2d696b3b7d6990c1562a
Eunji <hyni03@naver.com>
2025-03-31 00:45:46 +0900

    refactor: delete SurveyRepository file #95

--- backend/src/main/java/com/example/backend/dashboard/repository/SurveyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.dashboard.repository;
-
-import com.example.backend.common.domain.CaseEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SurveyRepository extends JpaRepository<CaseEntity, Integer> {
-
-}
