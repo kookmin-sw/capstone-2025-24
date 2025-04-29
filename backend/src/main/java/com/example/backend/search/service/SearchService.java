392449b033a2a2d08124928b6ca60508f0e0b0bb
mjk25 <winsomjk@gmail.com>
2025-04-29 17:22:04 +0900

    feat: get /log/{id} presigned video URL logic #160

--- backend/src/main/java/com/example/backend/search/service/SearchService.java
+++ backend/src/main/java/com/example/backend/search/service/SearchService.java
@@ -1,17 +1,23 @@
 package com.example.backend.search.service;
 
+import com.amazonaws.HttpMethod;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.example.backend.common.domain.CaseEntity;
 import com.example.backend.search.domain.SearchSpecification;
 import com.example.backend.search.dto.*;
 import com.example.backend.search.repository.SearchRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.*;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Service
@@ -58,11 +64,29 @@ public class SearchService {
                 .build();
     }
 
+    @Value("${cloud.aws.bucket}")
+    private String bucket;
+    private final AmazonS3 s3Client;
     public DetailResponse getLogById(Integer id) {
         CaseEntity caseEntity = searchRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 ID의 로그를 찾을 수 없습니다."));
 
-        return DetailResponse.fromEntity(caseEntity);
+        String videoKey = caseEntity.getVideo();
+
+        // Presigned URL 유효기간 설정 (30분)
+        Date expiration = new Date();
+        long expTime = expiration.getTime();
+        expTime += TimeUnit.MINUTES.toMillis(30);   // 30 minute
+        expiration.setTime(expTime);
+
+        GeneratePresignedUrlRequest presignRequest =
+                new GeneratePresignedUrlRequest(bucket, videoKey)
+                        .withMethod(HttpMethod.GET)
+                        .withExpiration(expiration);
+
+        String presignedUrl = s3Client.generatePresignedUrl(presignRequest).toString();
+
+        return DetailResponse.fromEntity(caseEntity, presignedUrl);
     }
 
 
